fa1ffa4afdccea85df66c6965d7d24d7b0921b09
GuillaumePB <55443646+GuillaumePB@users.noreply.github.com>
2022-04-21 11:28:54 -0400

    Update library.js
    ▢ Add a custom logging function to hide the output, when needed...
    ▢ Some cleanups...
    ▢ Add header credits...
    ▢ Normalize comments...

--- assets/js/library.js
+++ assets/js/library.js
@@ -1,8 +1,11 @@
-// Display init
-console.log('✅ External libraries loaded correctly.');
-
-// Command reference (for creating the zip):
-// zip -r -X myzip-FR.zip *
+/*
+ * Surveying tool generator - Université Laval
+ * ------------------------------------
+ * Copyright (C) 2022 Université Laval - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license, which unfortunately won't be
+ * written for another century.
+ */
 
 var pdfURL;
 var pdfFileName = 'dc81a165fb79.pdf';
@@ -23,34 +26,32 @@ var docSize;
 var charLimit;
 var language = "french";
 
-var rootFolderName = "Package";
+var rootFolderName = uniqueID + "_Package";
 var activitieDirName;
 var activityArr = [];
+
+// Swith to hide console logging
+// (Debug mode)
+var msgSwitch = true;
+// Display init...
+DisplayMessage('✅ External libraries loaded correctly.');
 
 // Main container zip
 var mainZip = new JSZip();
 
-// Format file size
-function returnFileSize(number) {
-  if(number < 1024) {
-    return number + 'bytes';
-  } else if(number >= 1024 && number < 1048576) {
-    return (number/1024).toFixed(1) + 'KB';
-  } else if(number >= 1048576) {
-    return (number/1048576).toFixed(1) + 'MB';
-  }
-}
+// ****************************************
 
-// References
+// Form UI References
 // Generate button
 var generateBtn = document.querySelector("body > main > section > section > form > button");
+
+// Language dropdown
+var dropDown = document.querySelector("body > main > section > section > form > div:nth-child(2) > div > div:nth-child(3) > select");
 
 // File input
 var fileInput = document.querySelector("body > main > section > section > form > div:nth-child(3) > div > div > input");
 
-// Disable the generate button on loading
-// generateBtn.setAttribute('disabled', true);
-// generateBtn.setAttribute('className', 'btn btn-secondary btn-lg');
+// ****************************************
 
 fileInput.addEventListener('change', function (e) {
 
@@ -72,7 +73,7 @@ fileInput.addEventListener('change', function (e) {
        var content = readerEvent.target.result; // this is the content!
 
        // Preview the dataurl
-       // console.log(content);
+       // DisplayMessage(content);
 
        // Assign the data URL to global variable
        pdfURL = content;
@@ -139,17 +140,31 @@ async function CreateFolderName() {
     activityArr.push(endPoint);
 
     // preview the name assemblage...
-    console.log(activityArr);
+    DisplayMessage(activityArr);
 
-
-
-    //
+    // Create the list, to display in the modal (for validation feedback)
     document.querySelector("#exampleModalCenter > div > div > div.modal-body").innerHTML = AssembleModalList(activityArr);
 
     // Download the files
     DownloadFiles();
 }
 
+// ****************************************
+
+// Helper function
+
+// 1) Format file size
+function returnFileSize(number) {
+  if(number < 1024) {
+    return number + 'bytes';
+  } else if(number >= 1024 && number < 1048576) {
+    return (number/1024).toFixed(1) + 'KB';
+  } else if(number >= 1048576) {
+    return (number/1048576).toFixed(1) + 'MB';
+  }
+}
+
+// 2) Assemble the file list
 function AssembleModalList(myFiles) {
     var listStr = 'Cela peut prendre quelques minutes... Vous avec le temps \nd\'aller chercher un bon café! ☕️😉<br><br>📁 Package.zip:<br>';
     for (var i = 0; i < myFiles.length; i++) {
@@ -158,11 +173,9 @@ function AssembleModalList(myFiles) {
     return listStr;
 }
 
+// 3) Get the selected language
 function GetSelectedLanguage() {
-
     var selectedLang;
-    var dropDown = document.querySelector("body > main > section > section > form > div:nth-child(2) > div > div:nth-child(3) > select");
-
     if (dropDown.value == '12') {
         selectedLang = 'french';
     } else {
@@ -171,74 +184,59 @@ function GetSelectedLanguage() {
     return selectedLang;
 }
 
+// 4) Custom logging
+function DisplayMessage(content) {
+    if (msgSwitch == true) {
+        console.log(content);
+    } else {
+        return;
+    }
+}
 
 // ****************************************
 
 function StartHeavyLifting(url) {
-    // ...
 
-    console.log('Retrieving the blob of PDF cover...');
+    DisplayMessage('Retrieving the blob of PDF cover...');
 
     var xhr = new XMLHttpRequest();
       xhr.responseType = 'blob';
       xhr.onload = function () {
         var a = document.createElement('a'); // create html element anchor
         a.href = window.URL.createObjectURL(xhr.response); // xhr.response is a blob
-
         // Get the blob
         docCoverURLBlob = xhr.response;
-
       };
-
       xhr.open('GET', url);
       xhr.send();
-
 }
 
 // ****************************************
 
 function DownloadFiles() {
-    // ...
 
-    console.log('Download Files...');
-
-    // Go to end Screen... (waiting room)
-    // player.SetVar('goToEndPoint', true);
+    DisplayMessage('Download Files...');
 
     var xhr = new XMLHttpRequest();
       xhr.responseType = 'blob';
       xhr.onload = function () {
         var a = document.createElement('a'); // create html element anchor
         a.href = window.URL.createObjectURL(xhr.response); // xhr.response is a blob
-
-        // Send the blob to the zipping process
-        // ZipFiles(xhr.response);
-
         // Retrieve the zip template...
         ReadTemplate(xhr.response);
-
-        // Uncomment this to trigger download
-        // a.download = pdfFileName; // Set the file name.
-        // a.style.display = 'none'; // set anchor as hidden
-        // document.body.appendChild(a);
-        // a.click();
-        // a.remove()
       };
-
       xhr.open('GET', pdfURL);
       xhr.send();
-
 }
 
 
 function ReadTemplate(pdffile) {
-
-   // Get the selected language version
-   if (language == 'french') {
+    // Get the .zip template according to the selected language
+    if (language == 'french') {
       selectedLanguageFilePath = zipTemplateFR;
-   } else {
+    } else {
       selectedLanguageFilePath = zipTemplateEN;
-   }
+    }
 
     var request = new XMLHttpRequest();
     request.open('GET', selectedLanguageFilePath, true);
@@ -248,7 +246,7 @@ function ReadTemplate(pdffile) {
         reader.readAsDataURL(request.response);
         reader.onload = async function(e){
 
-            // console.log('DataURL:', e.target.result);
+            // Set the subfolders names
             await SetSubFolderName(pdffile, request.response);
 
             // Save the files to the main zip
@@ -265,9 +263,7 @@ function SaveToMain() {
     // Save the main container
     mainZip.generateAsync({type:"blob"})
     .then(function(content) {
-        // see FileSaver.js
-        // saveAs(content, rootFolderName + ".zip");
-        // saveAs(content, "myCourse_XX98_1_4 - Storyline output" + ".zip");
+
         saveAs(content, rootFolderName + ".zip");
 
         // Move to end point and reset the mainZip
@@ -277,28 +273,23 @@ function SaveToMain() {
 
 }
 
-
+// Define the subfolers names, inside the main package.zip
 async function SetSubFolderName(mypdf, myzip) {
 
     for (var i = 0; i < activityArr.length; i++) {
         var sfn = activityArr[i] + ' - Storyline output';
-
         await AddFilesToTemplate(mypdf, myzip, sfn);
-
         }
-
 }
 
 // Promised return function
 function AddFilesToTemplate(mypdf, myzip, subFolderName) {
 
     return new Promise(function(resolve, reject) {
-        // ....
 
         // ****************************************
 
         var jsZip = new JSZip();
-        //var destinationJszIP = new JSZip();
 
         // Here we load the .zip used as a template, and read it
         jsZip.folder(subFolderName).loadAsync(myzip, {createFolders: true}).then(function (zip) {
@@ -360,8 +351,8 @@ function AddToMain(subfolder, ctn) {
 
 function MoveToEndpoint() {
 
-    console.log('Endpoint reached...');
-
+    DisplayMessage('Endpoint reached...');
+    // Reset the main zip...
     mainZip = new JSZip();
     activityArr = [];
     console.clear();
@@ -391,7 +382,7 @@ async function TextFieldsCount(doc) {
    for (var field of form.getFields()) { // Get all acroform fields.
 
       // Inspect the constructor.name of different field type
-      // console.log(field.constructor.name);
+      // DisplayMessage(field.constructor.name);
 
       if (field.constructor.name == "PDFTextField") {
          count += 1;
@@ -399,12 +390,8 @@ async function TextFieldsCount(doc) {
    }
 
    // Preview the number of fields
-   console.log(count);
-
-   // ****************************************
-
+   DisplayMessage("🔎 " + count + " Text fields found...");
    return count;
-
 }
 
 
@@ -412,29 +399,28 @@ async function TextFieldsCount(doc) {
 var pdfjsLib = window['pdfjs-dist/build/pdf'];
  pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://mozilla.github.io/pdf.js/build/pdf.worker.js';
 
- //
- // Asynchronous download PDF as an ArrayBuffer
+// Asynchronous download PDF as an ArrayBuffer
  var pdf = fileInput;
 
  pdf.onchange = function (ev) {
    if (file = pdf.files[0]) {
      fileReader = new FileReader();
      fileReader.onload = function (ev) {
-       console.log(ev);
+       DisplayMessage(ev);
 
        var loadingTask = pdfjsLib.getDocument(fileReader.result);
 
        loadingTask.promise
          .then(function (pdf) {
-           console.log('PDF loaded');
+           DisplayMessage('PDF loaded');
 
            // Fetch the first page
            var pageNumber = 1;
            pdf.getPage(pageNumber).then(function (page) {
-             console.log('Page loaded');
+             DisplayMessage('Page loaded');
 
              var scale = 0.8;
-             console.log(page);
+             DisplayMessage(page);
              var viewport = page.getViewport({ scale: scale });
              var canvas = document.createElement('canvas');
              // var canvas = document.getElementById('the-canvas');
@@ -450,7 +436,7 @@ var pdfjsLib = window['pdfjs-dist/build/pdf'];
              var renderTask = page.render(renderContext);
 
              renderTask.promise.then(function () {
-               // console.log(canvas.toDataURL('image/png'));
+               // DisplayMessage(canvas.toDataURL('image/png'));
                docCoverURL = canvas.toDataURL('image/png');
                canvas.style.visibility = 'hidden';
 
@@ -462,7 +448,7 @@ var pdfjsLib = window['pdfjs-dist/build/pdf'];
              });
            });
          }, function (error) {
-           console.log(error);
+           DisplayMessage(error);
          });
      };
      fileReader.readAsArrayBuffer(file);
